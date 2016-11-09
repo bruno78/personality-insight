8ed9bd0d688b69054e78abd7fc0dbae168543016
bruno78 <devbruno78@gmail.com>
2016-11-09 18:01:13 -0500

    added twitter and watson api

---
+++ ComparePersonality.py
@@ -0,0 +1,13 @@
+import sys
+import operator
+import requests
+import json
+import twitter
+from watson_developer_cloud import PersonalityInsightsV2 as PersonalityInsights
+import config
+
+
+# Interact with Twitter API
+twitter_api = twitter.Api(consumer_key = twitter_consumer_key, consumer_secret = twitter_consumer_secret,
+access_token_key = twitter_access_token,
+access_token_secret = twitter_access_secret)
